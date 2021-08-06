18d0c18bde91800fa07d615daba121ac323b6fd0
Ingrid Silva <ingridsilvatec@outlook.com>
2021-08-06 11:53:49 -0300

    cria diretorios

--- NYBooksApp/app/src/main/java/com/example/nybooksapp/presentation/books/BooksActivity.kt
+++ NYBooksApp/app/src/main/java/com/example/nybooksapp/presentation/books/BooksActivity.kt
@@ -1,11 +1,12 @@
-package com.example.nybooksapp
+package com.example.nybooksapp.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nybooksapp.R
 
-class MainActivity : AppCompatActivity() {
+class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_books)
     }
 }
