a5fcb8d346a76682acaa4ff45bf4b42aa3efe442
Ingrid Silva <ingridsilvatec@outlook.com>
2021-08-09 18:27:13 -0300

    implementa kapt moshi, constroi vmodel com as informações da API

--- NYBooksApp/app/src/main/java/com/example/nybooksapp/data/response/NYTServices.kt
+++ NYBooksApp/app/src/main/java/com/example/nybooksapp/data/response/NYTServices.kt
@@ -1,11 +1,13 @@
 package com.example.nybooksapp.data.response
 
-import com.example.nybooksapp.data.Book
 import retrofit2.Call
 import retrofit2.http.GET
-
+import retrofit2.http.Query
 
 interface NYTServices {
     @GET("lists.json")
-    fun listRepos(): Call<List<Book>>
+    fun getBooks(
+        @Query("api-key") apiKey : String ="SSLBb6NG0IVQw4GsWE3S3EPiXJ31O4mY",
+        @Query("list") list: String = "hardcover-fiction"
+    ): Call<BookBodyResponse>
 }
--- NYBooksApp/app/src/main/java/com/example/nybooksapp/presentation/books/BooksViewModel.kt
+++ NYBooksApp/app/src/main/java/com/example/nybooksapp/presentation/books/BooksViewModel.kt
@@ -3,20 +3,42 @@ package com.example.nybooksapp.presentation.books
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.nybooksapp.data.Book
+import com.example.nybooksapp.data.response.ApiService
+import com.example.nybooksapp.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class BooksViewModel : ViewModel() {
 
     val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
-
     fun getBooks() {
-        booksLiveData.value = createFakeBooks()
-    }
+        ApiService.service.getBooks().enqueue(object : Callback<BookBodyResponse> {
+            override fun onResponse(
+                call: Call<BookBodyResponse>,
+                response: Response<BookBodyResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val books: MutableList<Book> = mutableListOf()
+                    response.body()?.let {
+                        for (result in it.bookResults) {
+                            val book = Book(
+                                title = result.bookDetailsResponse[0].title,
+                                author = result.bookDetailsResponse[0].author
+                            )
+                            books.add(book)
+                        }
+                    }
+                    booksLiveData.value = books
+                }
+            }
 
-    fun createFakeBooks(): List<Book> {
-        return listOf(
-            Book("Livro 1", "Autor1"),
-            Book("Livro 2", "Autor2"),
-            Book("Livro 3", "Autor3")
-        )
+            override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+
     }
 }
+
